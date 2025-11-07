7f965253be4c2216209c8efb9171f4a3a6dc2d2f
Aditya Narayan <aditya.narayan3104@gmial.com>
2025-11-07 23:36:03 +0530

    refactor

--- frontend-nextjs/next.config.ts
+++ frontend-nextjs/next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
-import path from "path";
-
 const nextConfig: NextConfig = {
   /* config options here */
-  turbopack: {
-    root: path.resolve(__dirname),
-  },
 };
 
 export default nextConfig;
